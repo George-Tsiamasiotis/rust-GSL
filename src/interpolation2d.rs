ce3bf96ce4afa7d807f46c9c8cee0148e4add2e5
George-Tsiamasiotis <gt.develop@protonmail.com>
2025-07-16 11:51:17 +0300

    check: added eval_extrap and eval_extrap_e

--- src/interpolation2d.rs
+++ src/interpolation2d.rs
@@ -70,3 +70,66 @@ pub fn eval_e(
     };
     Error::handle(ret, z)
 }
+
+/// This function returns the interpolated value of z for a given point (x, y), using the
+/// interpolation object interp, data arrays xa, ya, and za and the accelerators xacc and yacc. The
+/// functions perform no bounds checking, so when x is outside the range of xa or y is outside the
+/// range of ya, extrapolation is performed.
+#[doc(alias = "gsl_interp2d_eval_extrap")]
+pub fn eval_extrap(
+    interp2d: &crate::Interp2d,
+    xa: &[f64],
+    ya: &[f64],
+    za: &[f64],
+    x: f64,
+    y: f64,
+    xacc: &mut crate::InterpAccel,
+    yacc: &mut crate::InterpAccel,
+) -> f64 {
+    unsafe {
+        sys::gsl_interp2d_eval_extrap(
+            interp2d.unwrap_shared(),
+            xa.as_ptr(),
+            ya.as_ptr(),
+            za.as_ptr(),
+            x,
+            y,
+            &mut xacc.0,
+            &mut yacc.0,
+        )
+    }
+}
+
+/// This function returns the interpolated value of z for a given point (x, y), using the
+/// interpolation object interp, data arrays xa, ya, and za and the accelerators xacc and yacc. The
+/// functions perform no bounds checking, so when x is outside the range of xa or y is outside the
+/// range of ya, extrapolation is performed.
+///
+/// Returns 'z'.
+#[doc(alias = "gsl_interp2d_eval_extrap_e")]
+pub fn eval_extrap_e(
+    interp2d: &crate::Interp2d,
+    xa: &[f64],
+    ya: &[f64],
+    za: &[f64],
+    x: f64,
+    y: f64,
+    xacc: &mut crate::InterpAccel,
+    yacc: &mut crate::InterpAccel,
+) -> Result<f64, Error> {
+    let mut z = 0.;
+    let ret = unsafe {
+        sys::gsl_interp2d_eval_extrap_e(
+            interp2d.unwrap_shared(),
+            xa.as_ptr(),
+            ya.as_ptr(),
+            za.as_ptr(),
+            x,
+            y,
+            &mut xacc.0,
+            &mut yacc.0,
+            &mut z,
+        )
+    };
+    Error::handle(ret, z)
+}
